150259618dd3af3fa18f3eb3515c08db13c19ec6
31385427 <31385427@099068-P31S202.macklabs.br>
2016-03-28 08:56:27 -0300

    Fazer um contrutor vazio

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -22,8 +22,10 @@ public class TicketMachine {
     }
 
     TicketMachine() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      
     }
+
+    
 
    
     public void setValor(int valor) {
